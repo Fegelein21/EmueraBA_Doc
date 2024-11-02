9818c97ee33baa244bca9db6ee1b68c5fdef875e
Fegelein21 <weixiaodongfm@foxmail.com>
2024-11-03 00:06:11 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://fegelein21.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/EmueraBA_Doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
